4d5cb81903e22d181a8a2549147917835e9cf56d
John Karahalis <john.karahalis@gmail.com>
2015-03-22 08:48:16 -0700

    Add comments to fix-filenames.sh
    This updates the script to closely mirror the version posted to Gist:
    https://gist.github.com/openjck/e4ff62090702b01e04d9

--- scripts/fix-filenames.sh
+++ scripts/fix-filenames.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# Settings
+WEBROOT='../www'
+
 # Rename [dirname]/index.[ext] to [dirname].[ext]
 #
 # For example:
 # misc/drupal/index.js -> misc/drupal.js
 #
-# This addresses one shortcoming in the HTTrack approach that was used the
-# archive the site[1], which is that all files are renamed to index.[ext]. This
-# corrects the issue by restoring the correct filenames and updating all
-# references accordingly.
+# This addresses one shortcoming in the webikon article about using HTTrack to
+# make a static archive of a website.[1] When the steps in the article are
+# followed, all files are renamed to index.[ext], even if they are not HTML
+# documents. This script corrects the issue by restoring the original filenames
+# and updating all references accordingly.
+#
+# The steps in the webikon article[1] should be followed before running this
+# script.
+#
+# This worked perfectly for me, but I only tested it on Linux and only used it
+# to archive a relatively simple Drupal site. YMMV.
 #
 # [1] http://webikon.com/cases/park-your-old-drupal-site
 
@@ -16,7 +26,6 @@
 # iterate over files that contain spaces and handle them as expected.
 IFS=$'\n'
 
-WEBROOT='../www'
 WRONG_FILENAMES=`find $WEBROOT -type f -name 'index.*' -not -name 'index.html'`
 
 for WRONG_FILENAME in $WRONG_FILENAMES; do
@@ -31,5 +40,6 @@ for WRONG_FILENAME in $WRONG_FILENAMES; do
     rmdir $CORRECT_BASENAME &> /dev/null
 done
 
+# Update all references
 find $WEBROOT -type f -exec sed -i 's/\/index\.\([a-zA-Z]*\)/.\1/g' {} \;
 find $WEBROOT -type f \( -name '*.css' -o -name '*.js' \) -exec sed -i 's/\.\.\///' {} \;
